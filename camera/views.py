d117623d418da084feb3f3b5003572aad9461b32
DoHee-Kim <88043302+Doren1@users.noreply.github.com>
2023-06-03 18:09:45 +0900

    모델 올리기 성공 ..?
    Former-commit-id: 72ee68d0dc0b83396094c92cbef56918d09a86f7

--- camera/views.py
+++ camera/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.views.decorators import gzip
-from django.http import StreamingHttpResponse
 import cv2
+import numpy as np
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+from django.http import StreamingHttpResponse
+from camera.models import MyModel
 import threading
-import time
 
-# Create your views here.
+# 모델 경로와 디바이스 설정
+model = MyModel()
+device = model.device
+model.model.to(device)
+model.model.eval()
 
-#카메라 관련 클래스
+# 카메라 관련 클래스
 class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
@@ -18,30 +26,89 @@ class VideoCamera(object):
         self.video.release()
 
     def get_frame(self):
-        image = self.frame
+        image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)  # OpenCV의 BGR 형식을 RGB로 변환
         _, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
 
     def update(self):
         while True:
-            (self.grabbed, self.frame) = self.video.read()
+            (self.grabbed, self.frame) = self.video.read()  # 프레임을 여기서 업데이트합니다
+            if not self.grabbed:
+                break
 
 
-def gen(camera):
+# 프레임 처리 및 예측 함수
+def process_frame(frame, model):
+    # 프레임 전처리
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_tensor = transform(frame_rgb)
+    # frame_tensor = transform(frame)
+    if frame_tensor.shape[0] == 1:
+        frame_tensor = frame_tensor.expand(3, -1, -1, -1)
+
+    # Add a batch dimension
+    frame_tensor = frame_tensor.unsqueeze(0)
+
+    # Move the tensor to the appropriate device
+    frame_tensor = frame_tensor.to(device)
+
+    # 모델 예측
+    with torch.no_grad():
+        # Check the number of channels and expand if necessary
+        if frame_tensor.shape[0] == 1:
+            frame_tensor = frame_tensor.expand(3, -1, -1, -1)
+            frame_tensor = frame_tensor.unsqueeze(0)  # Add batch dimension
+        else:
+            frame_tensor = frame_tensor.unsqueeze(0)  # Add batch dimension
+
+        output = model.model(frame_tensor)
+
+
+    # 예측 결과 가져오기
+    _, predicted_idx = torch.max(output.data, 1)
+    predicted_class = predicted_idx.item()
+    print(predicted_class)
+
+    return predicted_class
+
+# 비디오 프레임을 가져오고 예측 결과를 반환하는 함수
+def get_video_frame(cam, model):
     while True:
-        frame = camera.get_frame()
-        # frame단위로 이미지를 계속 반환한다. (yield)
-        yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        frame = cam.get_frame()
+        frame_np = np.frombuffer(frame, np.uint8)  # 바이트 데이터를 NumPy 배열로 변환
+        frame_img = cv2.imdecode(frame_np, cv2.IMREAD_COLOR)  # NumPy 배열을 이미지로 디코딩
+        frame_img_rgb = cv2.cvtColor(frame_img, cv2.COLOR_BGR2RGB)
+        predicted_class = process_frame(frame_img_rgb, model)
 
+        # 예측 결과 표시
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame_img_rgb, f"Predicted Class: {predicted_class}", (10, 30), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-# detectme를 띄우는 코드(여기서 웹캠을 킨다.)
-@gzip.gzip_page
+        ret, jpeg = cv2.imencode('.jpg', frame_img_rgb)
+        frame_bytes = jpeg.tobytes()
+
+        # 프레임 단위로 이미지 반환
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n\r\n')
+
+# 프레임 및 예측 결과를 생성하는 제너레이터
+# def gen(camera, model):
+#     for frame in get_video_frame(camera, model):
+#         # 프레임 단위로 이미지 반환
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+# 웹캠 영상 스트리밍
 def camera(request):
     try:
-        cam = VideoCamera() #웹캠 호출
-        # frame단위로 이미지를 계속 송출한다
-        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
-    except:  # This is bad! replace it with proper handling
-        print("에러입니다...")
+        cam = VideoCamera()  # 웹캠 호출
+        return StreamingHttpResponse(get_video_frame(cam, model), content_type="multipart/x-mixed-replace;boundary=frame")
+    except Exception as e:
+        print("에러입니다:", str(e))
         pass
